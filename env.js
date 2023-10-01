483786c5902533ad4057b701b86cba57c31effbb
EdithFelicia <78738551+EdithFelicia@users.noreply.github.com>
2023-10-01 21:37:57 +0700

    Add files via upload

---
+++ env.js
@@ -0,0 +1,14 @@
+// 1. Membuat Env Variable sesuai data yang diregister
+
+const cat_env = {
+  API_KEY: "live_oRpGvWd0HpiunrUaYsoizcV1xaJwBYHiBUZjxkXjUPLAD5jzai1kbgbOYpEyrQIg",
+  endpoint: "https://api.thecatapi.com/",
+};
+
+const dog_env = {
+  API_KEY: "live_APQqJDMnGuyraNOEPfCM3Akp1TpuypEOpvYblSYiITf6gsWl37OsOVqRGhVD3QFI",
+  endpoint: "https://api.thedogapi.com/",
+};
+
+// 2. Export dog_env
+export default dog_env;
